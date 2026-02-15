e1280c3c1d0731416202a6c73c28c2c836d0b384
ahmad.altal@bidayah.vc <ahmad.tal.dev@gmail.com>
2026-02-15 08:39:18 +0300

    test-one

--- App.tsx
+++ App.tsx
@@ -39,7 +39,7 @@ const AuthRoutes = () => {
 
   return (
     <Routes>
-      {/* <Route path="/login" element={<Login />} /> */}
+      <Route path="/login" element={<Login />} />
       <Route
         path="/dashboard"
         element={
